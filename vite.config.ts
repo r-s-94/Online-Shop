228f4a516f1172b7fde9318daad6fbd5c1baef01
r-s-94 <119122869+r-s-94@users.noreply.github.com>
2024-05-26 16:53:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/r-s-94/Online-Shop//",
+  base: "/https://r-s-94/Online-Shop//",
   plugins: [react()],
 });
