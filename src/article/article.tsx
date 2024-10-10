91ed7d6002d3b7f9c9c7d227a12a052f0bd07d9d
r.s. <richter.s.94@web.de>
2024-10-10 16:21:14 +0200

    Add gh action

--- src/article/article.tsx
+++ src/article/article.tsx
@@ -56,9 +56,22 @@ export default function ArticleComponent({
             className="article__description-section--button-div--link-component link-component"
           >
             <button className="article__description-section--button-div--link-component--more-info-button button">
-              mehr Infos{" "}
+              mehr Infos
               <span className="article__description-section--button-div--link-component--more-info-button--icon">
-                &#128712;
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                  />
+                </svg>
               </span>
             </button>
           </Link>{" "}
